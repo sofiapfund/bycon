af539fa23394495a1a696e917f3d4a001dc8a5b4
Sofia <79449314+sofiapfund@users.noreply.github.com>
2021-03-16 20:36:07 +0100

    Update publication_parser.py

--- byconeer/lib/publication_parser.py
+++ byconeer/lib/publication_parser.py
@@ -8,10 +8,18 @@ def generate_publication_label(pub):
 
     if "authors" in pub:
         pa = pub["authors"].copy()
+        title = pub["title"].copy()
+        year = pub["year"].copy()
+        
+        lab = pa[:50] + f' et al. ({year}): ' 
+      
+        if len(title) <= 100:
+            label = lab + title
+        else:
+            label = lab + ' '.join(title.split(' ')[:12]) + ' ...'
+        
+        #label_no_html = re.sub(r'<[^\>]+?>', "", label) #is the formatting necessary or are title, authors, etc. already formatted before?
 
-    # ...
-
-    
     pub.update({"label": label})
 
     return pub
